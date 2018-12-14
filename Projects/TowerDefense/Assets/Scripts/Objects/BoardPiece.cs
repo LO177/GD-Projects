dc75ce6f30be10be56fafea6e7a9701b76b21cf1
LO177 <l017081h@student.staffs.ac.uk>
2018-12-14 14:54:17 +0000

    Board piece setup for TowerDefence project

---
+++ Projects/TowerDefense/Assets/Scripts/Objects/BoardPiece.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BoardPiece : MonoBehaviour {
+
+    protected MeshRenderer m_Renderer;
+
+    protected MeshFilter m_Filter;
+
+    BoardPieceTemplate m_Template;
+
+    public BoardPieceTemplate Template { get { return m_Template; } }
+
+    public void Init(BoardPieceTemplate template)
+    {
+        m_Template = template;
+
+        m_Renderer = GetComponent<MeshRenderer>();
+        m_Filter = GetComponent<MeshFilter>();
+        PlaceObject();
+    }
+
+    public void PlaceObject()
+    {
+        transform.localPosition = Vector3.zero;
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Projects/TowerDefense/Assets/Scripts/Objects/BoardPieceTemplate.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BoardPiece Template", menuName = "Tower Defence/Board Piece Template")]
+public class BoardPieceTemplate : ScriptableObject {
+
+    [SerializeField]
+    string m_DisplayName;
+    
+    public string DisplayName { get { return m_DisplayName; } }
+
+    [SerializeField]
+    int m_BuildCost;
+
+    public int BuildCost { get { return m_BuildCost; } }
+
+    [SerializeField]
+    Sprite m_BuildUISprite;
+
+    public Sprite BuildUISprite { get { return m_BuildUISprite; } }
+
+    [SerializeField]
+    BoardPiece m_BoardPiece;
+
+    public BoardPiece SpawnBoardPiece(Transform parent)
+    {
+        BoardPiece temp = Instantiate(m_BoardPiece);
+
+        temp.transform.SetParent(parent);
+
+        temp.Init(this);
+
+        return temp;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
